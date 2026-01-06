16d74dd67dac019bb41189c99a615fa4f18b0e31
FerdigVibes <100457062+FerdigVibes@users.noreply.github.com>
2026-01-05 18:48:59 -0600

    Update render.js

--- docs/assets/js/render.js
+++ docs/assets/js/render.js
@@ -1,1 +1,16 @@
+import { state } from "./state.js";
 
+export function renderApp() {
+  renderTopBar();
+  renderPokedexList();
+}
+
+function renderTopBar() {
+  const title = document.getElementById("title");
+  title.textContent = `Pokédex – ${state.game.toUpperCase()}`;
+}
+
+function renderPokedexList() {
+  const list = document.getElementById("pokedex-list");
+  list.innerHTML = "<p>Pokédex list coming next.</p>";
+}
