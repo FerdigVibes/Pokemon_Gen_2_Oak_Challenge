024b97c612b845b5aadb314697e0f39ba41a25a6
William Ferdig <100457062+FerdigVibes@users.noreply.github.com>
2026-01-26 15:36:48 -0600

    Create location.js

---
+++ docs/data/maps/location.js
@@ -0,0 +1,35 @@
+export const LOCATION_REGISTRY = {
+  // ===== JOHTO =====
+  "Ilex Forest": {
+    map: "johto",
+    x: 18,
+    y: 82
+  },
+  "National Park": {
+    map: "johto",
+    x: 32,
+    y: 55
+  },
+  "Union Cave": {
+    map: "johto",
+    x: 30,
+    y: 90
+  },
+
+  // ===== KANTO =====
+  "Pokémon Tower": {
+    map: "kanto",
+    x: 70,
+    y: 18
+  },
+  "Viridian Forest": {
+    map: "kanto",
+    x: 45,
+    y: 30
+  },
+  "Mt. Moon": {
+    map: "kanto",
+    x: 60,
+    y: 20
+  }
+};
